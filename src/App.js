1d99bf282279d9ea24c2297ab789a8f5d9654f0e
tripott <tripott@gmail.com>
2019-06-20 14:22:27 -0400

    010-pet-jsx

--- src/App.js
+++ src/App.js
@@ -2,22 +2,33 @@ import React from 'react'
 import { render } from 'react-dom'
 import Pet from './Pet'
 
+// const App = () => {
+//   return React.createElement('div', {}, [
+//     React.createElement('h1', {}, 'Adopt Me!'),
+//     React.createElement(Pet, { name: 'Trigger', animal: 'Dog', breed: 'Mutt' }),
+//     React.createElement(Pet, { name: 'Dash', animal: 'Dog', breed: 'Mutt' }),
+//     React.createElement(Pet, {
+//       name: 'Tootles',
+//       animal: 'Dog',
+//       breed: 'Minx'
+//     }),
+//     React.createElement(Pet, {
+//       name: 'Freddy',
+//       animal: 'Frog',
+//       breed: 'American Bullfrog'
+//     })
+//   ])
+// }
+
 const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Adopt Me!'),
-    React.createElement(Pet, { name: 'Trigger', animal: 'Dog', breed: 'Mutt' }),
-    React.createElement(Pet, { name: 'Dash', animal: 'Dog', breed: 'Mutt' }),
-    React.createElement(Pet, {
-      name: 'Tootles',
-      animal: 'Dog',
-      breed: 'Minx'
-    }),
-    React.createElement(Pet, {
-      name: 'Freddy',
-      animal: 'Frog',
-      breed: 'American Bullfrog'
-    })
-  ])
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Tucker" animal="Dog" breed="Carolina Ditch Dog" />
+      <Pet name="Dash" breed="Terrier" />
+      <Pet name="Tootles" animal="Cat" breed="Tabby" />
+    </div>
+  )
 }
 
 render(React.createElement(App), document.getElementById('root'))
--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, name),
-    React.createElement('h2', {}, animal),
-    React.createElement('h2', {}, breed)
-  ])
+// const Pet = ({ name, animal, breed }) => {
+//   return React.createElement('div', {}, [
+//     React.createElement('h1', {}, name),
+//     React.createElement('h2', {}, animal),
+//     React.createElement('h2', {}, breed)
+//   ])
+// }
+
+const Pet = ({ name, animal = 'None provided', breed = 'None provided' }) => {
+  return (
+    <div>
+      <h1>Name: {name}</h1>
+      <h2>Animal: {animal}</h2>
+      <h3>Breed: {breed}</h3>
+    </div>
+  )
 }
 
 export default Pet
