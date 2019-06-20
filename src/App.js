672122bcb906faec4f18906d784a5a1a8ff8cb9c
tripott <tripott@gmail.com>
2019-06-20 10:54:44 -0400

    005-pet-props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-const Pet = () => {
+const Pet = props => {
     return React.createElement("div", {}, [
-        React.createElement("h1", {}, "Tucker"),
-        React.createElement("h2", {}, "Dog"),
-        React.createElement("h2", {}, "Carolina Porch Dog")
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed)
     ])
 }
 
 const App = () => {
     return React.createElement("div", {}, [
         React.createElement("h1", {}, "Adopt Me!"),
-        React.createElement(Pet),
-        React.createElement(Pet),
-        React.createElement(Pet),
-        React.createElement(Pet)
+        React.createElement(Pet, {name: "Tucker", animal:"Dog", breed: "Mutt"}),
+        React.createElement(Pet, {name: "Dash", animal:"Dog", breed: "Mutt"}),
+        React.createElement(Pet, {name: "Tootles", animal:"Cat", breed: "Tabby"}),
+        React.createElement(Pet, {name: "Freddy", animal:"Frog", breed: "American Bullfrog"})
     ])
 }
 
