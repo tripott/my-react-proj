4d4a9f03f85381f3cf14ca2bc061404f181cdefc
tripott <tripott@gmail.com>
2019-06-20 14:05:40 -0400

    009-move-pet

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
-
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, name),
-    React.createElement('h2', {}, animal),
-    React.createElement('h2', {}, breed)
-  ])
-}
+import Pet from './Pet'
 
 const App = () => {
   return React.createElement('div', {}, [
     React.createElement('h1', {}, 'Adopt Me!'),
-    React.createElement(Pet, { name: 'Tom', animal: 'Dog', breed: 'Mutt' }),
+    React.createElement(Pet, { name: 'Trigger', animal: 'Dog', breed: 'Mutt' }),
     React.createElement(Pet, { name: 'Dash', animal: 'Dog', breed: 'Mutt' }),
     React.createElement(Pet, {
       name: 'Tootles',
