df83cfc8e1721a9949e1df2ea9ba4bdf370ad6e2
tripott <tripott@gmail.com>
2019-06-21 11:06:48 -0400

    016-pretty-pet

--- src/Pet.js
+++ src/Pet.js
@@ -8,13 +8,27 @@ import React from 'react'
 //   ])
 // }
 
-const Pet = ({ name, animal = 'None provided', breed = 'None provided' }) => {
+const Pet = ({
+  name,
+  animal = 'None provided',
+  breed = 'None provided',
+  media,
+  location,
+  id,
+  key
+}) => {
+  const hero = media.length ? media[0].small : 'http://placecorgi.com/300/300'
+
   return (
-    <div>
-      <h1>Name: {name}</h1>
-      <h2>Animal: {animal}</h2>
-      <h3>Breed: {breed}</h3>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name}></img>
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </a>
   )
 }
 
